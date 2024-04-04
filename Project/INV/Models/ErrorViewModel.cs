4c07237860b6de6dbc5589e149a67f2a4108549a
Freddy Arroyo <farroyo@ecatrans.com>
2024-04-04 15:22:44 -0600

    Add Project Empty

---
+++ Project/INV/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace INV.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
